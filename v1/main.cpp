7d77590d03333d12f50345b50047cd098d2e840e
Bobislav <bobo.svilenov@gmail.com>
2024-02-01 11:59:04 +0200

    particle on screen

--- v1/main.cpp
+++ v1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
@@ -8,12 +9,18 @@
 
 float deltaTime;
 
+
 int main() 
 {
-    deltaTime = 0.1;
+    deltaTime = 0.01;
+    
+    std::vector<Particle> particles;
+
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "empty window", sf::Style::Titlebar | sf::Style::Close);
     sf::Event ev;
+
+    particles.push_back(Particle(100.0, 100.0, 0.1, 0.0, 20.0));
 
     while (window.isOpen()) 
     {
@@ -37,13 +44,14 @@ int main()
         }
 
         //renderer
-        window.clear(); //clear frame
+        window.clear(sf::Color::White); //clear frame
 
         //render HERE & game code
         for (int point = 0; point < particles.size(); point++)
         {
+            particles[point].update(deltaTime);
             particles[point].draw();
-            window.draw(particles[point]);
+            window.draw(particles[point].sprite);
         }
 
         window.display(); //show new frame
--- v1/objects/Particle.cpp
+++ v1/objects/Particle.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
-#include <vector>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 
-std::vector<Particle> particles;
-
 class Particle
 {
 private:
+
+public:
     sf::Vector2f posC;
     sf::Vector2f posL;
     sf::Vector2f accel;
-    sf::CircleShape sprite;
     sf::Vector2f vel;
+    sf::CircleShape sprite;
 
-public:
     Particle(float x, float y, float Vx, float Vy, int rad) : posC(x, y), posL(x - Vx, y - Vy), accel(0.0, 0.1)
     {
         sprite.setRadius(rad);
@@ -30,10 +28,13 @@ public:
         posC += vel + accel * dt * dt;
     }
 
+    void bounds(int radius)
+    {
+       
+    }
+
     void draw()
     {
         sprite.setPosition(posC);
     }
 };
-
-

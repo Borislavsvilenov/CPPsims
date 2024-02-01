16ed39d9048fc1793377c3533bcdc0c07e5029ea
Bobislav <bobo.svilenov@gmail.com>
2024-02-01 20:59:29 +0200

    v2 window

---
+++ v2/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+
+int main()
+{   
+    sf::RenderWindow window(sf::VideoMode(800, 800), "empty window", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event ev;
+
+    sf::Clock clock;
+    sf::Time deltaTime;
+
+    while(window.isOpen())
+    {
+        while (window.pollEvent(ev))
+        {
+            switch (ev.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::KeyPressed:
+                if (ev.key.code == sf::Keyboard::Escape)
+                {
+                    window.close();
+                }
+                break;  
+
+            default:
+                break;
+            }
+        }
+    }
+
+
+    return 0;
+}

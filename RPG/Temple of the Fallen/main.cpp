f863c0268367b9fbaaf7e4de5ec9d7849c37d7b0
Bobislav <bobo.svilenov@gmail.com>
2024-02-07 13:53:04 +0200

    rpg

---
+++ RPG/Temple of the Fallen/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+int main () {
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SIM AREA", sf::Style::Titlebar | sf::Style::Close);
+    window.setFramerateLimit(60);
+    sf::Event ev;
+
+    while(window.isOpen())
+    {
+        while (window.pollEvent(ev))
+        {
+            switch (ev.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::KeyPressed:
+                if (ev.key.code == sf::Keyboard::Escape)
+                {
+                    window.close();
+                }
+                break;  
+
+            default:
+                break;
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+
+        window.display();
+
+    }
+
+    return 0;
+}
